66bb7825a03397ba385b0487839efed59748b229
tatomr <34355847+tatomr@users.noreply.github.com>
2018-03-08 21:26:52 -0500

    Add files via upload

---
+++ DataRetrivalSQL/DataRetrivalSQL/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace DataRetrivalSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Loadbutton1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(572, 271);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Loadbutton1
+            // 
+            this.Loadbutton1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.Loadbutton1.Location = new System.Drawing.Point(277, 289);
+            this.Loadbutton1.Name = "Loadbutton1";
+            this.Loadbutton1.Size = new System.Drawing.Size(75, 23);
+            this.Loadbutton1.TabIndex = 1;
+            this.Loadbutton1.Text = "Load Data";
+            this.Loadbutton1.UseVisualStyleBackColor = true;
+            this.Loadbutton1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 324);
+            this.Controls.Add(this.Loadbutton1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Loadbutton1;
+    }
+}
+
---
+++ DataRetrivalSQL/DataRetrivalSQL/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DataRetrivalSQL
+{
+    public partial class Form1 : Form
+       
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Define connection string
+            const string connString = @"Server=PL13\MTCDBDEV; Database=AdventureWorks2012;Trusted_Connection=True;";
+            try
+            {
+                // Define the connection using the connection string.
+                SqlConnection sqlConn = new SqlConnection(connString);
+
+                // Define a data adapter to pull the data from the server using the connection and a stored procedure.
+                SqlDataAdapter sqlDA = new SqlDataAdapter("sp_VendorsAddresses", sqlConn);
+
+                // Declare an empty data table to hold the data.
+                DataTable dtEmployees = new DataTable();
+
+                // Fill the data table using the data adapter.
+                sqlDA.Fill(dtEmployees);
+
+                // Use the data table as the data source for a data grid control.
+                dataGridView1.DataSource = dtEmployees;
+            }
+            catch
+            {
+                MessageBox.Show("You done goofed up!");
+            }
+        }
+    }
+}
